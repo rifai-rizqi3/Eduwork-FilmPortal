446fa2ec70a0a1f4b939a3152e079bd0ad481633
RIZQI NUR RIFA'I <84585203+rifai-rizqi3@users.noreply.github.com>
2023-03-05 17:52:17 +0700

    update finish

--- src/App.js
+++ src/App.js
@@ -1,11 +1,40 @@
 
 import './App.css';
+import { getMovieList, searchMovie } from './api'
+import { useEffect, useState } from 'react';
 
 const App = () => {
+  const [popularMovies, setPopularMovies] = useState([])
 
-  const search = (q) => {
-    console.log({q})
+   useEffect (() => {
+      getMovieList().then((result) => {
+        setPopularMovies(result)
+      })
+    }, [])
+    
+const PopularMovieList = () => {
+  return popularMovies.map((movie, i) => {
+    return (
+          <div className="Movie-wrapper" key={i}>
+            <div className="Movie-title">{movie.title}</div>
+            <img 
+            className="Movie-image" 
+            src={`${process.env.REACT_APP_BASEIMGURL}/${movie.poster_path}`}/>
+            <div className="Movie-date">Release : {movie.release_date}</div>
+            <div className="Movie-rate">Rate : {movie.vote_average}</div>
+          </div>
+    )
+  })
+}
+
+  const search = async (q) => {
+    if (q.length> 3) {
+      const query = await searchMovie(q)
+      setPopularMovies(query.results)
+    }
   }
+
+
   return (
     <div className="App">
       <header className="App-header">
@@ -16,12 +45,7 @@ const App = () => {
           onChange={({target})=>search(target.value)}
           />
         <div className="Movie-container">
-          <div className="Movie-wrapper">
-            <div className="Movie-title">CONTOH PERTAMA</div>
-            <img className="Movie-image"></img>
-            <div className="Movie-date">12-12-1995</div>
-            <div className="Movie-rate">9</div>
-          </div>
+          <PopularMovieList />
         </div>
       </header>
     </div>
